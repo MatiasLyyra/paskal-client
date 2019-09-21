5308e2ec69f74e86e1e7d33a13a81a76425c0e05
ci-build <matias.lyyra@gmail.com>
2019-09-21 16:13:45 +0000

    [skip ci] Updates

---
+++ precache-manifest.e4900743056c717f879704944597616a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892ba342a57451aaaa21e28283f7abb5",
+    "url": "/paskal-client/index.html"
+  },
+  {
+    "revision": "a44d52a979690cfeb795",
+    "url": "/paskal-client/static/css/main.a0089bbc.chunk.css"
+  },
+  {
+    "revision": "766dd378c2f566795ccd",
+    "url": "/paskal-client/static/js/2.a56402d6.chunk.js"
+  },
+  {
+    "revision": "a44d52a979690cfeb795",
+    "url": "/paskal-client/static/js/main.93b4dc21.chunk.js"
+  },
+  {
+    "revision": "ca676784a9c9817169bc",
+    "url": "/paskal-client/static/js/runtime~main.af9225bf.js"
+  }
+]);
